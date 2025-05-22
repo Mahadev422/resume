d6e030badd28f4484753e68a0e8d0b3bb853fbd0
Mahadev <project9162595119@gmail.com>
2025-05-22 15:07:34 +0530

    link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const currentURL = window.location.href;
+  const currentURL = 'https://mahadev422.github.io/Portfolio/';
 
-  new QRCode(document.getElementById("qr-code"), {
+  new QRCode(document.getElementById("qr-code-portfolio"), {
       text: currentURL,
       width: 150,
       height: 150,
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
       correctLevel: QRCode.CorrectLevel.H
   });
 });
+
 function showModal(certType) {
     const modal = document.getElementById('certModal');
     const modalImage = document.getElementById('modalImage');
